ab1ab036a3fbda46cc7d169296a009bb5ae82b8c
bpronin <boris.i.pronin@gmail.com>
2021-06-09 00:14:13 +0300

    added basic functionality

--- app/src/main/java/com/bopr/piclock/MainActivity.kt
+++ app/src/main/java/com/bopr/piclock/MainActivity.kt
@@ -5,27 +5,38 @@ import android.content.SharedPreferences.OnSharedPreferenceChangeListener
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.widget.TextView
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import androidx.appcompat.app.AppCompatActivity
+import com.bopr.piclock.Settings.Companion.PREF_24_HOURS_FORMAT
+import com.bopr.piclock.Settings.Companion.PREF_MINUTES_SEPARATOR
+import com.bopr.piclock.Settings.Companion.PREF_MINUTES_SEPARATOR_BLINKING
+import com.bopr.piclock.Settings.Companion.PREF_SECONDS_SEPARATOR
+import com.bopr.piclock.Settings.Companion.PREF_SECONDS_VISIBLE
 import com.bopr.piclock.databinding.ActivityMainBinding
+import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
 
 class MainActivity : AppCompatActivity(), OnSharedPreferenceChangeListener {
 
-    private lateinit var hoursView: TextView
     private lateinit var binding: ActivityMainBinding
-    private lateinit var fullscreenSupport:FullscreenSupport
+    private lateinit var fullscreenSupport: FullscreenSupport
+    private lateinit var settings: Settings
     private val handler = Handler(Looper.getMainLooper())
-    private val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-    private val hoursFormat = SimpleDateFormat("HH:MM", Locale.getDefault())
-    private val secondsFormat = SimpleDateFormat("ss", Locale.getDefault())
+
+    private var hoursFormat24 = SimpleDateFormat("HH", Locale.getDefault())
+    private var hoursFormatAmPm = SimpleDateFormat("h", Locale.getDefault())
+    private var amPmFormat = SimpleDateFormat("a", Locale.getDefault())
+    private var minutesFormat = SimpleDateFormat("mm", Locale.getDefault())
+    private var secondsFormat = SimpleDateFormat("ss", Locale.getDefault())
+    private var dateFormat = SimpleDateFormat("EEEE, MMMM dd", Locale.getDefault())
 
     private val timerTask = object : Runnable {
 
         override fun run() {
-            updateTimeControls()
+            updateTimeViews()
             handler.postDelayed(this, 1000)
         }
     }
@@ -35,12 +46,18 @@ class MainActivity : AppCompatActivity(), OnSharedPreferenceChangeListener {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.root.setOnClickListener { onScreenTouch() }
+
         fullscreenSupport = FullscreenSupport(window)
 
-        hoursView = binding.hoursView
-        hoursView.setOnClickListener {
-            fullscreenSupport.fullscreen = !fullscreenSupport.fullscreen
-        }
+        settings = Settings(this)
+        settings.loadDefaults()
+        settings.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        settings.unregisterOnSharedPreferenceChangeListener(this)
     }
 
     override fun onPostCreate(savedInstanceState: Bundle?) {
@@ -66,10 +83,47 @@ class MainActivity : AppCompatActivity(), OnSharedPreferenceChangeListener {
         TODO("Not yet implemented")
     }
 
-    private fun updateTimeControls() {
-        val time = Date()
-        hoursView.text = hoursFormat.format(time)
+    private fun onScreenTouch() {
+        fullscreenSupport.fullscreen = !fullscreenSupport.fullscreen
     }
 
+    private fun updateTimeViews() {
+        val time = Date()
+        binding.run {
+            hoursView.text = getHoursFormat().format(time)
+            minutesView.text = minutesFormat.format(time)
+
+            minutesSeparator.visibility = getMinutesSeparatorVisibility(time)
+            minutesSeparator.text = settings.getString(PREF_MINUTES_SEPARATOR)
+
+            secondsSeparator.text = settings.getString(PREF_SECONDS_SEPARATOR)
+
+            secondsView.visibility = getSecondsVisibility()
+            secondsView.text = secondsFormat.format(time)
+
+            amPmMarker.visibility = getAmPmMarkerVisibility()
+            amPmMarker.text = amPmFormat.format(time)
+
+            dateView.text = dateFormat.format(time)
+        }
+    }
+
+    private fun getHoursFormat(): DateFormat {
+        return if (settings.getBoolean(PREF_24_HOURS_FORMAT)) hoursFormat24 else hoursFormatAmPm
+    }
+
+    private fun getAmPmMarkerVisibility(): Int {
+        return if (settings.getBoolean(PREF_24_HOURS_FORMAT)) INVISIBLE else VISIBLE
+    }
+
+    private fun getMinutesSeparatorVisibility(time: Date): Int {
+        return if (settings.getBoolean(PREF_MINUTES_SEPARATOR_BLINKING)
+            && (time.time / 1000 % 2 == 0L)
+        ) VISIBLE else INVISIBLE
+    }
+
+    private fun getSecondsVisibility(): Int {
+        return if (settings.getBoolean(PREF_SECONDS_VISIBLE)) VISIBLE else INVISIBLE
+    }
 
 }
--- app/src/main/java/com/bopr/piclock/Settings.kt
+++ app/src/main/java/com/bopr/piclock/Settings.kt
@@ -5,13 +5,27 @@ import android.content.Context.MODE_PRIVATE
 import com.bopr.piclock.util.SharedPreferencesWrapper
 
 class Settings(context: Context) : SharedPreferencesWrapper(
-    context.getSharedPreferences(sharedPreferencesName, MODE_PRIVATE)
+    context.getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE)
 ) {
+
+    fun loadDefaults() = update {
+        putInt(PREF_SETTINGS_VERSION, SETTINGS_VERSION)
+        putBooleanOptional(PREF_MINUTES_SEPARATOR_BLINKING, true)
+        putStringOptional(PREF_MINUTES_SEPARATOR, ":")
+        putBooleanOptional(PREF_SECONDS_VISIBLE, true)
+    }
+
     companion object {
 
-        var sharedPreferencesName = "com.bopr.android.smailer_preferences"
-
         private const val SETTINGS_VERSION = 1
+        const val SHARED_PREFERENCES_NAME = "com.bopr.piclock_preferences"
+
+        const val PREF_SETTINGS_VERSION = "settings_version" /* hidden */
+        const val PREF_MINUTES_SEPARATOR = "minutes_separator"
+        const val PREF_MINUTES_SEPARATOR_BLINKING = "minutes_separator_blinking"
+        const val PREF_SECONDS_SEPARATOR = "seconds_separator"
+        const val PREF_SECONDS_VISIBLE = "seconds_visible"
+        const val PREF_24_HOURS_FORMAT = "24_hours_format"
 
         const val PREF_HOURS_COLOR = "hours-color"
         const val PREF_BACKGROUND_COLOR = "background-color"
